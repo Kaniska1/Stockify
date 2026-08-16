541f67beba1365c29a8fbdd3306a88949675498b
Kaniska <kaniska.mitra@gmail.com>
2026-08-16 21:24:17 +0530

    fix case-sensitive UI imports for deployment

--- client/src/app/components/ui/index.ts
+++ client/src/app/components/ui/index.ts
@@ -1,5 +1,5 @@
-export { default as Badge } from "./Badge";
-export { default as Button } from "./Button";
+export { default as Badge } from "./badge";
+export { default as Button } from "./button";
 export { default as Container } from "./Container";
 export { default as GlassPanel } from "./GlassPanel";
 export { default as Logo } from "./Logo";
